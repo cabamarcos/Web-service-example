4927d10733bd9f662a08fe07f613efc2e6d496f4
cabamarcos <marcos.caballero.0203@gmail.com>
2023-04-28 10:18:18 +0200

    HEcho

--- ej5.py
+++ ej5.py
@@ -1,33 +1,10 @@
-import requests
-import xml.etree.ElementTree as ET
-
-# Definir la URL del servicio web
-url = "https://www.w3schools.com/xml/tempconvert.asmx?op=ConvertTemp"
-
-# Definir el valor de temperatura a convertir
-valor_temp = 20
-
-# Definir la unidad de temperatura original
-unidad_origen = "Celsius"
-
-# Definir la unidad de temperatura a la que se desea convertir
-unidad_destino = "Fahrenheit"
-
-# Definir los parámetros para la solicitud HTTP POST
-params = {"Temperature": valor_temp, "FromUnit": unidad_origen, "ToUnit": unidad_destino}
-
-# Realizar la solicitud HTTP POST al servicio web y obtener la respuesta
-response = requests.post(url, data=params)
-
-# Imprimir la respuesta completa de la solicitud HTTP POST
-print(response.content)
-
-# Obtener la respuesta del servicio web en formato XML
-xml_response = response.text
-
-# Analizar el XML para extraer el valor de temperatura convertido
-root = ET.fromstring(xml_response)
-valor_convertido = root.find(".//double").text
-
-# Imprimir el resultado de la conversión de temperatura
-print(f"{valor_temp} {unidad_origen} = {valor_convertido} {unidad_destino}")
+import zeep
+def main():
+    wsdl_url = "https://www.w3schools.com/xml/tempconvert.asmx?WSDL"
+    soap = zeep.Client(wsdl=wsdl_url)
+    result = soap.service.FahrenheitToCelsius(90)
+    print(result)
+    result = soap.service.CelsiusToFahrenheit(32)
+    print(result)
+if __name__ == '__main__':
+    main()
